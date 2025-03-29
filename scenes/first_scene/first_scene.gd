44871c01b67c0ed4dd51ff7708167f4dbeb1a486
RisrrGaming <akfoote@emory.edu>
2025-03-29 13:17:21 -0400

    01 First Scene - Done
    Finished 01 - First Scene (Clicker) video tutorial on Sable's Youtube channel

---
+++ scenes/first_scene/first_scene.gd
@@ -0,0 +1,22 @@
+class_name FirstScene
+extends Control
+## The first scene created in this project
+
+
+## idleons owned by the Player
+var idleons: int = 0
+var growth_rate: int = 5
+
+func _ready() -> void:
+	update_label()
+	(get_node("Button") as Button).pressed.connect(_on_button_pressed)
+	
+func _on_button_pressed() -> void:
+	update_idleons(growth_rate)
+	update_label()
+
+func update_idleons(x: int) -> void:
+	idleons += x
+	
+func update_label() -> void:
+	(get_node("IdleonsLabel") as Label).text = "Idleons: %s" %idleons
